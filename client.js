98a347b56946ba0de79808fc00f3378f4465fe7c
Cole Dahlen <coledahlen@gmail.com>
2022-12-02 18:51:37 -0600

    finished base mode

--- client.js
+++ client.js
@@ -1,8 +1,15 @@
 $(document).ready(onReady);
 let employees = [];
-
+let monthlyMoney = 0; 
 function onReady(){
     $('#submitButton').on('click', addEmployee);
+    $('body').on('click', '.deleteButton', removeMonthlyMoney)
+    $('body').on('click', '.deleteButton', deleteEmployee);
+}
+function render(){
+    if($('#monthlyMoney').text() > 20000){
+        $('h3').css({'background-color': 'red', 'font-size': '200%'});
+    }
 }
 function addEmployee(){
     let firstNameInput = $('#firstNameInput').val();
@@ -10,6 +17,9 @@ function addEmployee(){
     let idInput = $('#idInput').val();
     let titleInput = $('#titleNameInput').val();
     let annualSalaryInput = $('#annualSalaryInput').val();
+    let monthlySalary = annualSalaryInput / 12;
+    monthlyMoney += monthlySalary;
+    $('#monthlyMoney').text(Number(monthlyMoney))
     let newEmployee = {
         firstName: firstNameInput,
         lastName: lastNameInput,
@@ -18,19 +28,38 @@ function addEmployee(){
         annualSalary: Number(annualSalaryInput)
     }
     employees.push(newEmployee);
-    console.log(employees)
+    let employeesWeDontWantToDelete = [];
+    $('.newEmployeeTR').empty();
     for(let i = 0; i < employees.length; i++){
         $('table').append(`
-        <tr>
+        <tr class="newEmployeeTR">
             <th>${employees[i].firstName}</th>
             <th>${employees[i].lastName}</th>
             <th>${employees[i].id}</th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>${employees[i].title}</th>
+            <th class="annualSalaryTH">${employees[i].annualSalary}</th>
+            <th><button class="deleteButton">DELETE</button></th>
         </tr>
         `)
+    }
+    $('#firstNameInput').val('')
+    $('#lastNameInput').val('')
+    $('#idInput').val('')
+    $('#titleNameInput').val('')
+    $('#annualSalaryInput').val('')
+    render()
 }
 function deleteEmployee(){
-
+    let newEmployeRemove = $(this).parent().parent();
+    newEmployeRemove.remove();
+    render()
+}
+function removeMonthlyMoney(){
+    let newEmployeeAnnualSalaryRemove = $(this).parent().siblings('.annualSalaryTH').text();
+    console.log(newEmployeeAnnualSalaryRemove)
+    let newEmployeeSalaryValue = newEmployeeAnnualSalaryRemove
+    let monthlySalary = newEmployeeSalaryValue / 12;
+    monthlyMoney = monthlyMoney - monthlySalary;
+    $('#monthlyMoney').text(monthlyMoney);
+    render()
 }
